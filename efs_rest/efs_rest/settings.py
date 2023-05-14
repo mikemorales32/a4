71e826f622a656dc56d5148ae6040462d7fbfcf7
mikemorales32 <michael.morales@unomaha.edu>
2023-05-13 23:04:26 -0500

    update

--- efs_rest/efs_rest/settings.py
+++ efs_rest/efs_rest/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = '_7&8&_zafl#gn=5n2yq45%#*^zsq#69xf+jigrtdn%6r-bfuev'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['michaelmorales.pythonanywhere.com',
+ALLOWED_HOSTS = ['https://michaelmorales.pythonanywhere.com',
                  '127.0.0.1:8000',
                  '127.0.0.1'
                  ]
--- efs_vue/src/http/APIService.js
+++ efs_vue/src/http/APIService.js
@@ -1,7 +1,7 @@
   /* eslint-disable */
 import axios from 'axios';
 //const API_URL = 'https://michaelmorales.pythonanywhere.com';
-const API_URL = 'http://127.0.0.1:8000/';
+const API_URL = 'http://127.0.0.1:8000';
 
 export class APIService {
   constructor() {
